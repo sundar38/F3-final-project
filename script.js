15d9177c6e5e5cb935712b4a02f492f9f7f215ca
Penugonda <sundara.k.penugonda@accenture.com>
2023-03-06 19:39:26 +0530

    redirecting

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 // myCartProductArray = myProducts.filter((item)=> myCartIDs.includes(item.id))
 
 function login(){
-    window.location.href="./Login/index.html"
+    window.location.href="/Login/index.html"
 }
 function signup(){
     // window.location.href="http://127.0.0.1:5501/signup/index.html"
-    window.location.href="./signup/index.html"
+    window.location.href="/signup/index.html"
 }
